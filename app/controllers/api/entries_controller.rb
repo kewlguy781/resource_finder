5bf6d41cd138e66b7791d53b3952f74ab23220f6
Jon Roberts <kewlguy@gmail.com>
2020-10-10 10:50:41 -0600

    controller and route info added

--- app/controllers/api/entries_controller.rb
+++ app/controllers/api/entries_controller.rb
@@ -1,2 +1,75 @@
 class Api::EntriesController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :update]
+  before_action :set_entry, only: [:show, :update, :destroy]
+  before_action :set_category
+
+  def index
+    render json: @category.entries
+  end
+
+  def entry_all
+    render json: Entry.all
+  end
+
+  def show
+    render json: @entry
+  end
+    
+  def user_entries
+    render json: @current_user.entries
+  end
+
+  def new 
+    @comment = Entry.new
+  end
+
+  def create
+    entry = Entry.new(entry_params)
+    if entry.save
+      render json: entry
+    else
+      render json: entry.errors, status: 422
+    end
+  end
+
+  def update
+    if entry.update(entry_params)
+      render json: @entry
+    else
+      render json: @entry.errors, status: 422
+    end
+  end
+
+  def destroy
+    @entry.destroy
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:category_id])
+  end
+
+  def set_entry
+    @entry = Entry.find(params[:entry_id])
+  end
+
+  def entry_params
+    params
+    .require(:entry)
+    .permit(
+        :name, 
+        :address, 
+        :img, 
+        :city, 
+        :state, 
+        :email, 
+        :facebook, 
+        :wed, 
+        :phone, 
+        :desc, 
+      )
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,15 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
-    resources :things
-  end
+    
+    resources :categories do
+      resources :entries 
+      end
+    
+    resources :entries do
+      resources :comments
+      end
+    end
+
   get '*other', to: 'static#index'
 end
